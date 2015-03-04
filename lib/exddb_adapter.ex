80be996a7f9682c05dc73baa64e34f0e4b3fecfe
Roope Kangas <muhmi@iki.fi>
2015-03-04 15:17:45 +0200

    expect clauses

--- lib/exddb_adapter.ex
+++ lib/exddb_adapter.ex
@@ -2,14 +2,18 @@ defmodule Exddb.Adapter do
   use Behaviour
 
   @type t :: module
+  @type expect_clause_hash :: [expected: {String.t, term}]
+  @type expect_clause_hash_range :: [expected: term]
+  
+  @expect_clause ~w(expect_clause_hash | expect_clause_hash_range)a
 
   defcallback create_table(table_name :: String.t, key_spec :: String.t, key :: term, write_units :: integer, read_units :: integer) :: no_return
   defcallback delete_table(table_name :: String.t) :: no_return
 
   defcallback put_item(table_name :: String.t, key_spec :: term, item :: term) :: term
-  defcallback put_item(table_name :: String.t, key_spec :: term, item :: term, expect_not_exists :: term) :: term
+  defcallback put_item(table_name :: String.t, key_spec :: term, item :: term, expect_clause :: term) :: term
   
-  defcallback delete_item(table_name :: String.t, key_spec :: term, expect_exists :: term) :: term
+  defcallback delete_item(table_name :: String.t, key_spec :: term, expect_clause :: term) :: term
   
   defcallback get_item(table_name :: String.t, key_spec :: term) :: term
 
