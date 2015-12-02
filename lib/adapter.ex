86861f77adb42a447b68d9a1134b74a55b25a895
Roope Kangas <muhmi@iki.fi>
2015-12-02 22:10:54 +0200

    behaviour syntax

--- lib/adapter.ex
+++ lib/adapter.ex
@@ -1,26 +1,31 @@
 defmodule Exddb.Adapter do
-  use Behaviour
 
   @type t :: module
   @type expect_clause_hash :: [expected: {String.t, term}]
   @type expect_clause_hash_range :: [expected: term]
 
   @type return_ok :: {:ok, any} | {:error, any}
-  
+
   @expect_clause ~w(expect_clause_hash | expect_clause_hash_range)a
 
-  defcallback create_table(table_name :: String.t, key_spec :: String.t, key :: term, write_units :: integer, read_units :: integer) :: no_return
-  defcallback delete_table(table_name :: String.t) :: no_return
+  @doc ~s"""
+  Create a table with given key specification. Also sets the provisioned write and read units.
+  """
+  @callback create_table(table_name :: String.t, key_spec :: String.t, key :: term, write_units :: integer, read_units :: integer) :: no_return
 
-  defcallback list_tables(options :: []) :: :ok | []
+  @doc "Delete table, does not return any status information"
+  @callback delete_table(table_name :: String.t) :: no_return
 
-  defcallback put_item(table_name :: String.t, key_spec :: term, item :: term, expect_clause :: term) :: return_ok
+  @doc "List tables, returns a list of strings"
+  @callback list_tables(options :: []) :: :ok | []
 
-  defcallback query(table_name :: String.t, key_conditions :: term, options :: term) :: return_ok
-  
-  defcallback delete_item(table_name :: String.t, key_spec :: term, expect_clause :: term) :: return_ok
-  
-  defcallback get_item(table_name :: String.t, key_spec :: term) :: return_ok
+  @doc "https://github.com/gleber/erlcloud/blob/master/src/erlcloud_ddb2.erl#L1688"
+  @callback put_item(table_name :: String.t, key_spec :: term, item :: term, expect_clause :: term) :: return_ok
 
+  @callback query(table_name :: String.t, key_conditions :: term, options :: term) :: return_ok
+
+  @callback delete_item(table_name :: String.t, key_spec :: term, expect_clause :: term) :: return_ok
+
+  @callback get_item(table_name :: String.t, key_spec :: term) :: return_ok
 
 end
