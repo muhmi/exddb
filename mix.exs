c10e71608d23df7227b2a9a866fe2ee09230cce9
Roope Kangas <muhmi@iki.fi>
2015-12-02 21:37:59 +0200

    Bump version to match tags

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exddb.Mixfile do
 
   def project do
     [app: :exddb,
-     version: "0.0.3",
+     version: "0.1.3",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
